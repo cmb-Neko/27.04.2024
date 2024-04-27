0305726818101d2ad53156d5f36021e3d6ece54d
AnmrMe0w <167676085+AnmrMe0w@users.noreply.github.com>
2024-04-27 17:04:53 +0300

    lab_2

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	int num;
-	cin >> num;
-	(num % 2 == 0) ? cout << "true" << endl : cout << "false" << endl;
+	int num_1, num_2;
+	cin >> num_1 >> num_2;
+	(num_1 > num_2) ? cout << "first number is bigger" << endl : cout << "second number is bigger" << endl;
 }
